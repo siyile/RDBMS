f7262b8676d6ce6b02e455848137a984800a48b0
siyile <seayahh@outlook.com>
2020-02-20 19:08:53 -0800

    add more pseudo code

--- ix/ix.cc
+++ ix/ix.cc
@@ -22,6 +22,17 @@ RC IndexManager::closeFile(IXFileHandle &ixFileHandle) {
 }
 
 /*
+ * IF node is leaf node = <KEY, INDICATOR, RID>
+ * LID = (pageNum, slotNum)
+ * IF node is not leaf node = <KEY, INDICATOR, PAGE_NUM>
+ * INDICATOR include LEAVE_INDICATOR, DELETE_INDICATOR
+ *
+ * PAGE DESIGN
+ * [LEAVE_NODE, LEAVE_NODE, ...
+ *
+ *
+ *      ..., SLOT, SLOT, Free_Space, SLOT_NUM, NEXT_PAGE_NUM]
+ *
  * search to find the bucket to insert record
  *
  * IF the bucket is not full
@@ -32,6 +43,44 @@ RC IndexManager::closeFile(IXFileHandle &ixFileHandle) {
  *       move the ceil((L + 1) / 2) node to parent, repeat the process
  *
  * IF the root splits, threat it as if it has an empty parent, then split
+ */
+
+/*
+ * search from the pseudo root to find the leaves, save the intermediate page in a stack
+ *
+ * IF there is enough space for the node:
+ *      find the slot, right shift data & left shift dictionary, then insert, end
+ *
+ * WHILE there is not enough space for the node:
+ *      Copy original PAGE TO PAGE1
+ *      Set i = 0
+ *      WHILE (i <= ceil((L + 1) / 2)):
+ *          IF NOT found:
+ *              IF node > node_i:
+ *                  CONTINUE
+ *              ELSE:
+ *                  insert node into PAGE, i--, found = True
+ *          ELSE:
+ *              write node_i into PAGE
+ *          i++
+ *
+ *      Get the ceil((L + 1) / 2) node value from PAGE
+ *
+ *      PAGE2 = MALLOC(PAGE_SIZE)
+ *
+ *      IF is LEAVE_NODE:
+ *          Set PAGE2 next page num to PAGE's next page number
+ *          Set PAGE next page num to PAGE2
+ *
+ *      WHILE (i <= L):
+ *          IF found == FALSE && node < node_i:
+ *              insert node into PAGE2, i--, found = True
+ *          ELSE:
+ *              write node_i into PAGE2
+ *
+ *      node = create new node to insert into PARENT_PAGE
+ *      PAGE = read PARENT_PAGE from stack
+ *
  *
  *
  */
