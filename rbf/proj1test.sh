260113fb856e3a736de937e0f3099d578a459520
Siyi Le <seayahh@outlook.com>
2020-03-05 22:15:52 -0800

    add test.sh

---
+++ rbf/proj1test.sh
@@ -0,0 +1,31 @@
+make clean
+make
+./rbftest_01
+./rbftest_02
+./rbftest_03
+./rbftest_04
+./rbftest_05
+./rbftest_06
+./rbftest_07
+./rbftest_08
+./rbftest_08b
+./rbftest_09
+./rbftest_10
+./rbftest_11
+./rbftest_12
+./rbftest_13
+./rbftest_13b
+./rbftest_14
+./rbftest_15
+./rbftest_delete
+./rbftest_update
+./rbftest_p1
+./rbftest_p2
+./rbftest_p2b
+./rbftest_p2c
+./rbftest_p3
+./rbftest_p3b
+./rbftest_p4
+./rbftest_p5
+./rbftest_p6
+make clean
